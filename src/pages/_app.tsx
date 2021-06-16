c883a1b1f279407536e348b2c98551594727360d
Emmanuel Chambon <echambon@scaleway.com>
2021-06-16 15:22:07 +0200

    fix: correct theme injection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
+import { ThemeProvider } from '@emotion/react'
 import Head from 'components/Head'
 import TopBar from 'components/TopBar'
 import { AppProps } from 'next/app'
+import { Button, GlobalStyle, theme } from '@scaleway/ui'
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Button>Hello</Button>
       <Head />
       <TopBar />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
-export default MyApp
+export default App
