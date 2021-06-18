f6c110f9026f1f709bb5bb9ff16506f743b739d7
Matthias <mprost@scaleway.com>
2021-06-18 18:42:01 +0200

    fix: last fixes for responsive

--- src/pages/home/Introduction.tsx
+++ src/pages/home/Introduction.tsx
@@ -1,4 +1,13 @@
-import { Col, Box, Grid, Row, Typography, up } from '@scaleway/ui'
+import {
+  Col,
+  Box,
+  Grid,
+  Row,
+  Typography,
+  up,
+  Button,
+  Breakpoint,
+} from '@scaleway/ui'
 import Image from 'next/image'
 import introductionIllustration from '../../assets/illustrations/introduction.png'
 import Card from '../../components/Card'
@@ -7,7 +16,7 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const StyledTitleCol = styled(Col)`
-  ${up('xsmall', `order: 2`)} ${up('large', `order: 1;`)};
+  ${up('xsmall', `order: 2`)} ${up('medium', `order: 1;`)};
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -18,26 +27,29 @@ const StyledTitle = styled(Typography)`
     'xsmall',
     `font-size: 40px; margin-top: 8px; width: 100%; text-align: center;`,
   )};
+  ${up('medium', `font-size: 58px`)};
   ${up('large', `font-size: 75px;`)};
 `
 
 const StyledSubTitle = styled(Typography)`
   ${up('xsmall', `font-size: 24px; text-align: center;`)};
+  ${up('medium', `font-size: 35px; text-align: left;`)};
   ${up('large', `font-size: 45px; text-align: left;`)};
 `
 
 const StyledIllustrationCol = styled(Col)`
   ${up('xsmall', `order: 1;`)};
-  ${up('large', `order: 2;`)}
+  ${up('medium', `order: 2;`)}
   display: flex;
   justify-content: center;
 `
 
 const StyledImageBox = styled(Box)`
-  ${up('xsmall', `width: 65vw`)};
+  ${up('xsmall', `width: 65vw; max-width: 350px`)};
   ${up(
     'large',
     `width: 470px;
+    max-width: none;
     height: 374px`,
   )};
 `
@@ -46,7 +58,7 @@ const Introduction = (): JSX.Element => {
   return (
     <Grid mt={4}>
       <Row alignItems="center">
-        <StyledTitleCol xsmall={12} large={6}>
+        <StyledTitleCol xsmall={12} medium={6}>
           <StyledTitle color="primary">
             Scaleway <b>UI</b>
           </StyledTitle>
@@ -55,7 +67,7 @@ const Introduction = (): JSX.Element => {
             Component Library
           </StyledSubTitle>
         </StyledTitleCol>
-        <StyledIllustrationCol xmsall={12} large={6}>
+        <StyledIllustrationCol xmsall={12} medium={6}>
           <StyledImageBox>
             <Image
               src={introductionIllustration}
@@ -65,6 +77,30 @@ const Introduction = (): JSX.Element => {
           </StyledImageBox>
         </StyledIllustrationCol>
       </Row>
+      <Breakpoint down="medium">
+        <Row justifyContent="center" mt={4}>
+          <Button
+            name="github"
+            variant="secondary"
+            icon="github"
+            iconSize={40}
+            size="medium"
+            p={0}
+            to="https://github.com/scaleway/scaleway-ui"
+            target="_blank"
+          />
+          <Button
+            name="documentation"
+            to="https://react.ui.scaleway.com/"
+            target="_blank"
+            size="medium"
+            mx={2}
+          >
+            Documentation
+          </Button>
+        </Row>
+      </Breakpoint>
+
       <Row mt={7}>
         <Col xsmall={12} large={4}>
           <Card
