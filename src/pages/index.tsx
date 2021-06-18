6b7e154bb4f5e2c50a57752b27b44b15962fd059
Matthias <mprost@scaleway.com>
2021-06-18 11:22:05 +0200

    feat: add introduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
-import { Grid, Row, Col } from '@scaleway/ui'
+import { Grid, Row, Col, Typography } from '@scaleway/ui'
+import Image from 'next/image'
+import React from 'react'
+import introductionIllustration from 'assets/illustrations/introduction.png'
 
 const Home = (): JSX.Element => {
   return (
-    <Grid>
+    <Grid mt={4}>
       <Row>
-        <Col height={900}>I am the body</Col>
+        <Col>
+          <Typography fontSize={75} color="primary">
+            Scaleway UI
+          </Typography>
+          <Typography fontSize={45} color="lightViolet" lineHeight={1.2}>
+            Open Source <br />
+            Component Library
+          </Typography>
+        </Col>
+        <Col>
+          <Image
+            src={introductionIllustration}
+            alt="logo"
+            width={470}
+            height={374}
+          />
+        </Col>
       </Row>
     </Grid>
   )
