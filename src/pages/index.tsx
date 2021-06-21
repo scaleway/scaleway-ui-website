e34deb1bfbff8ff2b00b4af831e9e57b2ca21cd5
Matthias <mprost@scaleway.com>
2021-06-21 16:12:25 +0200

    Revert "fix: revert"
    This reverts commit 8b74f4d0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,12 @@
-import { Button } from '@scaleway/ui'
+import { Grid, Row, Col } from '@scaleway/ui'
 
 const Home = (): JSX.Element => {
   return (
-    <div>
-      <Button>Hello</Button>
-
-      <p>
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div>
-        <a href="https://nextjs.org/docs">
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn">
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a href="https://github.com/vercel/next.js/tree/master/examples">
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-          <h2>Deploy &rarr;</h2>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
-    </div>
+    <Grid>
+      <Row>
+        <Col height={900}>I am the body</Col>
+      </Row>
+    </Grid>
   )
 }
 
