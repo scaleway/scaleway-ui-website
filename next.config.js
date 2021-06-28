ad709ec81a308ac4d2a62d5e3659121dd5827d36
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2021-06-28 14:46:25 +0200

    feat(sentry): add sentry

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const withTM = require('next-transpile-modules')([
   'react-syntax-highlighter',
 ])
 
-module.exports = withTM({
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const moduleExports = withTM({
   poweredByHeader: false,
   // Next is kinda buggy and don't want to export into static if this is not set even if its empty and we don't use it
   images: {
@@ -13,3 +15,16 @@ module.exports = withTM({
   },
   compress: true,
 })
+
+const SentryWebpackPluginOptions = {
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  dryRun: !!process.env.SENTRY_DRY_RUN,
+  ignore: ['node_modules'],
+  include: '.',
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: true,
+  url: process.env.SENTRY_URL,
+}
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
