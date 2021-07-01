c44eb21a203990c80099b5c621c051b7b77a52d1
philibeaux <aphilibeaux@scaleway.com>
2021-07-01 10:43:51 +0200

    feat(sentry): add sentry (#13)

---
+++ sentry.config.js
@@ -0,0 +1,8 @@
+const { init } = require('@sentry/nextjs')
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+
+init({
+  dsn: SENTRY_DSN,
+  tracesSampleRate: 1.0,
+})
