f7caf24a91b2f5d6dcaac7882c7c850c42aaae7e
Emmanuel Chambon <echambon@scaleway.com>
2021-07-06 10:39:54 +0200

    fix: simpler config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withSentryConfig } = require('@sentry/nextjs')
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withTM = require('next-transpile-modules')([
-  '@scaleway/ui',
-  '@scaleway/random-name',
-  'react-syntax-highlighter',
-])
-
-const nextConfig = withTM({
-  compress: true,
-  // Next is kinda buggy and don't want to export into static if this is not set even if its empty and we don't use it
-  images: {
-    loader: 'imgix',
-    path: '',
-  },
-  poweredByHeader: false,
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-})
 
 const SentryWebpackPluginOptions = {
   authToken: process.env.SENTRY_AUTH_TOKEN,
@@ -32,10 +12,31 @@ const SentryWebpackPluginOptions = {
   url: process.env.SENTRY_URL,
 }
 
-const moduleExports = process.env.SENTRY_AUTH_TOKEN
-  ? withSentryConfig(nextConfig, SentryWebpackPluginOptions)
-  : nextConfig
+const withSentry = (nextConfig) => process.env.SENTRY_AUTH_TOKEN ? withSentryConfig(nextConfig, SentryWebpackPluginOptions) : nextConfig
 
-module.exports = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})(moduleExports)
+module.exports = withSentry(() => {
+  const plugins = [
+    require('next-transpile-modules')([
+      '@scaleway/ui',
+      '@scaleway/random-name',
+      'react-syntax-highlighter',
+    ]),
+    require('@next/bundle-analyzer')({
+      enabled: process.env.ANALYZE === 'true',
+    }),
+  ].filter(Boolean);
+
+  return plugins.reduce((acc, next) => next(acc), {
+    compress: true,
+    // Next is kinda buggy and don't want to export into static if this is not set even if its empty and we don't use it
+    images: {
+      loader: 'imgix',
+      path: '',
+    },
+    poweredByHeader: false,
+    reactStrictMode: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  });
+});
